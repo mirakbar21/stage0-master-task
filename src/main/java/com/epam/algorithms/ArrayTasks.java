abc22f82207b98b343983b9eeda9e5c39c230f65
Mirakbar Mirzokhidov <mirakbarmirzokhidov21@gmail.com>
2023-04-13 03:04:40 +0500

    test

--- src/main/java/com/epam/algorithms/ArrayTasks.java
+++ src/main/java/com/epam/algorithms/ArrayTasks.java
@@ -93,11 +93,28 @@ public class ArrayTasks {
      * arr = [1,-2, 3]      -> [1, 3] arr = [-1, -2, -3]   -> [] arr = [1, 2]         -> [1, 2]
      */
     public int[] getOnlyPositiveNumbers(int[] arr) {
-        int positiveNumbers = 0;
+        // count the number of positive numbers in the array
+        int count = 0;
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > 0) positiveNumbers++;
+            if (arr[i] > 0) {
+                count++;
+            }
         }
-        return null;
+
+        // create a new array with the same length as the count of positive numbers
+        int[] result = new int[count];
+
+        // iterate through the original array and add positive numbers to the new array
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                result[index] = arr[i];
+                index++;
+            }
+        }
+
+        // return the new array with only positive numbers
+        return result;
     }
 
     /**
@@ -110,7 +127,31 @@ public class ArrayTasks {
      * arr = [[3, 1, 2,], [3,2]] -> [[2, 3], [1, 2, 3]] arr = [[5, 4], [7]]       -> [[7], [4, 5]]
      */
     public int[][] sortRaggedArray(int[][] arr) {
-        return null;
+        // Sort the incoming one-dimensional arrays by their length using bubble sort
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j].length > arr[j + 1].length) {
+                    int[] temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        // Sort the numbers in each one-dimensional array in ascending order using bubble sort
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                for (int k = 0; k < arr[i].length - j - 1; k++) {
+                    if (arr[i][k] > arr[i][k + 1]) {
+                        int temp = arr[i][k];
+                        arr[i][k] = arr[i][k + 1];
+                        arr[i][k + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        return arr;
     }
 
 }
